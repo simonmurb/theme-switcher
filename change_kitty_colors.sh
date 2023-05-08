d23e8bca75881bceb686e328ceb2dde7f446d06f
Simon McGaming <94650978+simonmurb@users.noreply.github.com>
2023-05-08 11:46:29 +0200

    Update change_kitty_colors.sh

--- change_kitty_colors.sh
+++ change_kitty_colors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for socket in /tmp/kitty-*; do
-  kitty @ --to=unix:"$socket" set-colors -c '/home/taro/.config/kitty/Themes/style0.conf'
+  kitty @ --to=unix:"$socket" set-colors -c '~/.config/kitty/Themes/style0.conf'
 done
